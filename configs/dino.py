5a88a5c088738f100db5fe3027a097ba3240643f
makecent <luchongkai96@gmail.com>
2023-06-14 17:17:43 +0800

    move default_runtimes

--- configs/dino.py
+++ configs/dino.py
@@ -1,5 +1,5 @@
 _base_ = [
-    './thumos14.py', './default_runtime.py'
+    './thumos14.py'
 ]
 custom_imports = dict(imports=['my_modules'], allow_failed_imports=False)
 model = dict(
@@ -104,11 +104,31 @@ param_scheduler = [
         gamma=0.1)
 ]
 
-default_hooks = dict(
-    logger=dict(type='LoggerHook', interval=10),
-    checkpoint=dict(type='CheckpointHook', interval=20))
-vis_backends = [dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')]
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
 # USER SHOULD NOT CHANGE ITS VALUES.
 # base_batch_size = (8 GPUs) x (2 samples per GPU)
 auto_scale_lr = dict(base_batch_size=16)
+
+default_scope = 'mmdet'
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=10),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', interval=20),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='DetVisualizationHook'))
+
+env_cfg = dict(
+    cudnn_benchmark=False,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'),
+)
+
+vis_backends = [dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')]
+visualizer = dict(
+    type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
+log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
+
+log_level = 'INFO'
+load_from = None
+resume = False
