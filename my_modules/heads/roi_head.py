086b589f26bdeaee93a3abc7f5a091d126d127e7
makecent <luchongkai96@gmail.com>
2023-06-18 17:29:13 +0800

    now the RoI head can be deactivated

--- my_modules/heads/roi_head.py
+++ my_modules/heads/roi_head.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import warnings
 from typing import List
 
 import torch
@@ -9,6 +10,7 @@ from mmdet.utils import ConfigType, InstanceList, OptMultiConfig
 from mmengine.model import BaseModule
 from torch import nn
 
+
 @MODELS.register_module()
 class MyRoIHead(BaseModule):
     """Simplest base roi head including one bbox head and one mask head."""
@@ -18,22 +20,27 @@ class MyRoIHead(BaseModule):
                  actionness_loss: ConfigType,
                  bbox_head: ConfigType,
                  init_cfg: OptMultiConfig = None,
-                 expand_roi_factor = 1.5,
+                 expand_roi_factor=1.5,
+                 active=True,  # experimental arguments, set to False can deactivate the RoI
                  **kwargs) -> None:
         super().__init__(init_cfg=init_cfg)
-        self.bbox_roi_extractor = MODELS.build(bbox_roi_extractor)
-        self.roi_size = bbox_roi_extractor['roi_layer']['output_size'][1]
-        self.dim = bbox_roi_extractor['out_channels']
-        self.expand_roi_factor = expand_roi_factor
-        self.actionness_fc = nn.Sequential(
-            nn.Flatten(start_dim=1),
-            nn.Linear(self.roi_size * self.dim, self.dim),
-            nn.ReLU(inplace=True),
-            nn.Linear(self.dim, self.dim),
-            nn.ReLU(inplace=True),
-            nn.Linear(self.dim, 1),
-            nn.Sigmoid())
-        self.actionness_loss = MODELS.build(actionness_loss)
+        self.active = active
+        if active:
+            self.bbox_roi_extractor = MODELS.build(bbox_roi_extractor)
+            self.roi_size = bbox_roi_extractor['roi_layer']['output_size'][1]
+            self.dim = bbox_roi_extractor['out_channels']
+            self.expand_roi_factor = expand_roi_factor
+            self.actionness_fc = nn.Sequential(
+                nn.Flatten(start_dim=1),
+                nn.Linear(self.roi_size * self.dim, self.dim),
+                nn.ReLU(inplace=True),
+                nn.Linear(self.dim, self.dim),
+                nn.ReLU(inplace=True),
+                nn.Linear(self.dim, 1),
+                nn.Sigmoid())
+            self.actionness_loss = MODELS.build(actionness_loss)
+        else:
+            warnings.warn("Please note that the RoIHead is now deactivated, no RoI will be applied")
 
         bbox_head.update(kwargs)
         self.bbox_head = MODELS.build(bbox_head)
@@ -58,12 +65,13 @@ class MyRoIHead(BaseModule):
         bbox_head_loss, bbox_pred = self.bbox_head.loss_and_predict(batch_data_samples=batch_data_samples,
                                                                     rescale=False,
                                                                     **head_inputs_dict)
-        actionness_pred = self.actionness_forward(memory, bbox_pred, batch_data_samples)
+        if self.active:
+            actionness_pred = self.actionness_forward(memory, bbox_pred, batch_data_samples)
 
-        actionness_target = self.get_actionness_target(bbox_pred, batch_data_samples)
-        actionness_loss = self.actionness_loss(actionness_pred.reshape(-1),
-                                               torch.cat(actionness_target, 0))
-        bbox_head_loss.update(dict(actionness_loss=actionness_loss))
+            actionness_target = self.get_actionness_target(bbox_pred, batch_data_samples)
+            actionness_loss = self.actionness_loss(actionness_pred.reshape(-1),
+                                                   torch.cat(actionness_target, 0))
+            bbox_head_loss.update(dict(actionness_loss=actionness_loss))
 
         return bbox_head_loss
 
@@ -72,9 +80,11 @@ class MyRoIHead(BaseModule):
         bbox_pred = self.bbox_head.predict(batch_data_samples=batch_data_samples,
                                            rescale=False,
                                            **head_inputs_dict)
-        actionness_pred = self.actionness_forward(memory, bbox_pred, batch_data_samples)
-
-        return self.post_process(bbox_pred, actionness_pred.reshape(len(bbox_pred), -1), batch_data_samples, rescale)
+        if self.active:
+            actionness_pred = self.actionness_forward(memory, bbox_pred, batch_data_samples).reshape(len(bbox_pred), -1)
+        else:
+            actionness_pred = [pred.scores for pred in bbox_pred]
+        return self.post_process(bbox_pred, actionness_pred, batch_data_samples, rescale)
 
     def actionness_forward(self, memory, bbox_pred, batch_data_samples):
         # Fix the y1, y2
@@ -124,5 +134,5 @@ class MyRoIHead(BaseModule):
                 pred.bboxes /= pred.bboxes.new_tensor(
                     img_meta['scale_factor']).repeat((1, 2))
                 # using actionness regression results as confidence scores of bboxes instead of classification score
-                pred.scores *= actionness
+                pred.scores = torch.sqrt(pred.scores * actionness)
         return bbox_pred
