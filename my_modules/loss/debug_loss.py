657fa8522956fa7491ac2dfe344144d895c752bf
makecent <luchongkai96@gmail.com>
2023-06-26 22:07:20 +0800

    ff

--- my_modules/loss/debug_loss.py
+++ my_modules/loss/debug_loss.py
@@ -36,7 +36,7 @@ def py_sigmoid_focal_loss(inputs, targets, weight, alpha: float = 0.25, gamma: f
         alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
         loss = alpha_t * loss
 
-    return loss.view(8, 40, 20).mean(1).sum() / avg_factor * 40
+    return loss.view(-1, 40, 20).mean(1).sum() / avg_factor * 40
 
 
 @MODELS.register_module(force=True)
