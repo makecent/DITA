0d247528e5c8074e46691d4707ba51e388d884ab
makecent <luchongkai96@gmail.com>
2023-06-27 14:11:41 +0800

    xx: further use TadTR IoU loss

--- my_modules/loss/debug_loss.py
+++ my_modules/loss/debug_loss.py
@@ -1,15 +1,15 @@
-# import torch
-# import torch.nn.functional as F
-# from mmdet.registry import MODELS
-# from torch import nn
-# from torch import Tensor
-# from typing import Optional
-# import warnings
-# from mmdet.models.losses.utils import weighted_loss
-#
-# # Just for debugging purpose
-#
-#
+import torch
+import torch.nn.functional as F
+from mmdet.registry import MODELS
+from torch import nn
+from torch import Tensor
+from typing import Optional
+import warnings
+from mmdet.models.losses.utils import weighted_loss
+
+# Just for debugging purpose
+
+
 # def py_sigmoid_focal_loss(inputs, targets, weight, alpha: float = 0.25, gamma: float = 2, avg_factor=None, reduction='mean'):
 #     """
 #     Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
@@ -37,9 +37,9 @@
 #         loss = alpha_t * loss
 #
 #     return loss.view(-1, 40, 20).mean(1).sum() / avg_factor * 40
-#
-#
-# # @MODELS.register_module(force=True)
+
+
+# @MODELS.register_module(force=True)
 # class FocalLoss(nn.Module):
 #
 #     def __init__(self,
@@ -125,162 +125,162 @@
 #         else:
 #             raise NotImplementedError
 #         return loss_cls
-#
-# def segment_length(segments):
-#     return (segments[:, 1]-segments[:, 0]).clamp(min=0)
-#
-# def segment_iou(segments1, segments2):
-#     """
-#     Temporal IoU between
-#
-#     The boxes should be in [x0, y0, x1, y1] format
-#
-#     Returns a [N, M] pairwise matrix, where N = len(segments1)
-#     and M = len(segments2)
-#     """
-#     # degenerate boxes gives inf / nan results
-#     # so do an early check
-#     assert (segments1[:, 1] >= segments1[:, 0]).all()
-#
-#     area1 = segment_length(segments1)
-#     area2 = segment_length(segments2)
-#
-#     l = torch.max(segments1[:, None, 0], segments2[:, 0])  # N,M
-#     r = torch.min(segments1[:, None, 1], segments2[:, 1])  # N,M
-#     inter = (r - l).clamp(min=0)  # [N,M]
-#
-#     union = area1[:, None] + area2 - inter
-#
-#     iou = inter / union
-#
-#     return iou
-#
-#
+
+def segment_length(segments):
+    return (segments[:, 1]-segments[:, 0]).clamp(min=0)
+
+def segment_iou(segments1, segments2):
+    """
+    Temporal IoU between
+
+    The boxes should be in [x0, y0, x1, y1] format
+
+    Returns a [N, M] pairwise matrix, where N = len(segments1)
+    and M = len(segments2)
+    """
+    # degenerate boxes gives inf / nan results
+    # so do an early check
+    assert (segments1[:, 1] >= segments1[:, 0]).all()
+
+    area1 = segment_length(segments1)
+    area2 = segment_length(segments2)
+
+    l = torch.max(segments1[:, None, 0], segments2[:, 0])  # N,M
+    r = torch.min(segments1[:, None, 1], segments2[:, 1])  # N,M
+    inter = (r - l).clamp(min=0)  # [N,M]
+
+    union = area1[:, None] + area2 - inter
+
+    iou = inter / union
+
+    return iou
+
+
 # @MODELS.register_module(force=True)
-# class IoULoss(nn.Module):
-#     """IoULoss.
-#
-#     Computing the IoU loss between a set of predicted bboxes and target bboxes.
-#
-#     Args:
-#         linear (bool): If True, use linear scale of loss else determined
-#             by mode. Default: False.
-#         eps (float): Epsilon to avoid log(0).
-#         reduction (str): Options are "none", "mean" and "sum".
-#         loss_weight (float): Weight of loss.
-#         mode (str): Loss scaling mode, including "linear", "square", and "log".
-#             Default: 'log'
-#     """
-#
-#     def __init__(self,
-#                  linear: bool = False,
-#                  eps: float = 1e-6,
-#                  reduction: str = 'mean',
-#                  loss_weight: float = 1.0,
-#                  mode: str = 'log') -> None:
-#         super().__init__()
-#         assert mode in ['linear', 'square', 'log']
-#         if linear:
-#             mode = 'linear'
-#             warnings.warn('DeprecationWarning: Setting "linear=True" in '
-#                           'IOULoss is deprecated, please use "mode=`linear`" '
-#                           'instead.')
-#         self.mode = mode
-#         self.linear = linear
-#         self.eps = eps
-#         self.reduction = reduction
-#         self.loss_weight = loss_weight
-#
-#     def forward(self,
-#                 pred: Tensor,
-#                 target: Tensor,
-#                 weight: Optional[Tensor] = None,
-#                 avg_factor: Optional[int] = None,
-#                 reduction_override: Optional[str] = None,
-#                 **kwargs) -> Tensor:
-#         """Forward function.
-#
-#         Args:
-#             pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
-#                 shape (n, 4).
-#             target (Tensor): The learning target of the prediction,
-#                 shape (n, 4).
-#             weight (Tensor, optional): The weight of loss for each
-#                 prediction. Defaults to None.
-#             avg_factor (int, optional): Average factor that is used to average
-#                 the loss. Defaults to None.
-#             reduction_override (str, optional): The reduction method used to
-#                 override the original reduction method of the loss.
-#                 Defaults to None. Options are "none", "mean" and "sum".
-#
-#         Return:
-#             Tensor: Loss tensor.
-#         """
-#         assert reduction_override in (None, 'none', 'mean', 'sum')
-#         reduction = (
-#             reduction_override if reduction_override else self.reduction)
-#         if (weight is not None) and (not torch.any(weight > 0)) and (
-#                 reduction != 'none'):
-#             if pred.dim() == weight.dim() + 1:
-#                 weight = weight.unsqueeze(1)
-#             return (pred * weight).sum()  # 0
-#         if weight is not None and weight.dim() > 1:
-#             # TODO: remove this in the future
-#             # reduce the weight of shape (n, 4) to (n,) to match the
-#             # iou_loss of shape (n,)
-#             assert weight.shape == pred.shape
-#             weight = weight.mean(-1)
-#         loss = self.loss_weight * iou_loss(
-#             pred,
-#             target,
-#             weight,
-#             mode=self.mode,
-#             eps=self.eps,
-#             reduction=reduction,
-#             avg_factor=avg_factor,
-#             **kwargs)
-#         return loss
-#
-# def iou_loss(pred: Tensor,
-#              target: Tensor,
-#              weight: Optional[Tensor] = None,
-#              linear: bool = False,
-#              mode: str = 'log',
-#              reduction: str = 'mean',
-#              avg_factor: Optional[int] = None,
-#              eps: float = 1e-6) -> Tensor:
-#     """IoU loss.
-#
-#     Computing the IoU loss between a set of predicted bboxes and target bboxes.
-#     The loss is calculated as negative log of IoU.
-#
-#     Args:
-#         pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
-#             shape (n, 4).
-#         target (Tensor): Corresponding gt bboxes, shape (n, 4).
-#         linear (bool, optional): If True, use linear scale of loss instead of
-#             log scale. Default: False.
-#         mode (str): Loss scaling mode, including "linear", "square", and "log".
-#             Default: 'log'
-#         eps (float): Epsilon to avoid log(0).
-#
-#     Return:
-#         Tensor: Loss tensor.
-#     """
-#     assert mode in ['linear', 'square', 'log']
-#     if linear:
-#         mode = 'linear'
-#         warnings.warn('DeprecationWarning: Setting "linear=True" in '
-#                       'iou_loss is deprecated, please use "mode=`linear`" '
-#                       'instead.')
-#     ious = torch.diag(segment_iou(pred[:, ::2], target[:, ::2]))
-#     # ious = bbox_overlaps(pred, target, is_aligned=True).clamp(min=eps)
-#     if mode == 'linear':
-#         loss = 1 - ious
-#     elif mode == 'square':
-#         loss = 1 - ious**2
-#     elif mode == 'log':
-#         loss = -ious.log()
-#     else:
-#         raise NotImplementedError
-#     return loss[weight != 0].sum() / avg_factor
+class IoULoss(nn.Module):
+    """IoULoss.
+
+    Computing the IoU loss between a set of predicted bboxes and target bboxes.
+
+    Args:
+        linear (bool): If True, use linear scale of loss else determined
+            by mode. Default: False.
+        eps (float): Epsilon to avoid log(0).
+        reduction (str): Options are "none", "mean" and "sum".
+        loss_weight (float): Weight of loss.
+        mode (str): Loss scaling mode, including "linear", "square", and "log".
+            Default: 'log'
+    """
+
+    def __init__(self,
+                 linear: bool = False,
+                 eps: float = 1e-6,
+                 reduction: str = 'mean',
+                 loss_weight: float = 1.0,
+                 mode: str = 'log') -> None:
+        super().__init__()
+        assert mode in ['linear', 'square', 'log']
+        if linear:
+            mode = 'linear'
+            warnings.warn('DeprecationWarning: Setting "linear=True" in '
+                          'IOULoss is deprecated, please use "mode=`linear`" '
+                          'instead.')
+        self.mode = mode
+        self.linear = linear
+        self.eps = eps
+        self.reduction = reduction
+        self.loss_weight = loss_weight
+
+    def forward(self,
+                pred: Tensor,
+                target: Tensor,
+                weight: Optional[Tensor] = None,
+                avg_factor: Optional[int] = None,
+                reduction_override: Optional[str] = None,
+                **kwargs) -> Tensor:
+        """Forward function.
+
+        Args:
+            pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
+                shape (n, 4).
+            target (Tensor): The learning target of the prediction,
+                shape (n, 4).
+            weight (Tensor, optional): The weight of loss for each
+                prediction. Defaults to None.
+            avg_factor (int, optional): Average factor that is used to average
+                the loss. Defaults to None.
+            reduction_override (str, optional): The reduction method used to
+                override the original reduction method of the loss.
+                Defaults to None. Options are "none", "mean" and "sum".
+
+        Return:
+            Tensor: Loss tensor.
+        """
+        assert reduction_override in (None, 'none', 'mean', 'sum')
+        reduction = (
+            reduction_override if reduction_override else self.reduction)
+        if (weight is not None) and (not torch.any(weight > 0)) and (
+                reduction != 'none'):
+            if pred.dim() == weight.dim() + 1:
+                weight = weight.unsqueeze(1)
+            return (pred * weight).sum()  # 0
+        if weight is not None and weight.dim() > 1:
+            # TODO: remove this in the future
+            # reduce the weight of shape (n, 4) to (n,) to match the
+            # iou_loss of shape (n,)
+            assert weight.shape == pred.shape
+            weight = weight.mean(-1)
+        loss = self.loss_weight * iou_loss(
+            pred,
+            target,
+            weight,
+            mode=self.mode,
+            eps=self.eps,
+            reduction=reduction,
+            avg_factor=avg_factor,
+            **kwargs)
+        return loss
+
+def iou_loss(pred: Tensor,
+             target: Tensor,
+             weight: Optional[Tensor] = None,
+             linear: bool = False,
+             mode: str = 'log',
+             reduction: str = 'mean',
+             avg_factor: Optional[int] = None,
+             eps: float = 1e-6) -> Tensor:
+    """IoU loss.
+
+    Computing the IoU loss between a set of predicted bboxes and target bboxes.
+    The loss is calculated as negative log of IoU.
+
+    Args:
+        pred (Tensor): Predicted bboxes of format (x1, y1, x2, y2),
+            shape (n, 4).
+        target (Tensor): Corresponding gt bboxes, shape (n, 4).
+        linear (bool, optional): If True, use linear scale of loss instead of
+            log scale. Default: False.
+        mode (str): Loss scaling mode, including "linear", "square", and "log".
+            Default: 'log'
+        eps (float): Epsilon to avoid log(0).
+
+    Return:
+        Tensor: Loss tensor.
+    """
+    assert mode in ['linear', 'square', 'log']
+    if linear:
+        mode = 'linear'
+        warnings.warn('DeprecationWarning: Setting "linear=True" in '
+                      'iou_loss is deprecated, please use "mode=`linear`" '
+                      'instead.')
+    ious = torch.diag(segment_iou(pred[:, ::2], target[:, ::2]))
+    # ious = bbox_overlaps(pred, target, is_aligned=True).clamp(min=eps)
+    if mode == 'linear':
+        loss = 1 - ious
+    elif mode == 'square':
+        loss = 1 - ious**2
+    elif mode == 'log':
+        loss = -ious.log()
+    else:
+        raise NotImplementedError
+    return loss[weight != 0].sum() / avg_factor
