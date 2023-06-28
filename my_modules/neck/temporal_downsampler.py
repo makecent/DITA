7645261ab5214ea5ca3bed1cb0cc7398255050cc
makecent <luchongkai96@gmail.com>
2023-06-28 14:58:46 +0800

    xx: add option mask in DownSampler1D to support masked 1d convolution if needed, default false.

--- my_modules/neck/temporal_downsampler.py
+++ my_modules/neck/temporal_downsampler.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 from mmcv.cnn import ConvModule
 from mmdet.registry import MODELS
+from torch.nn import functional as F
 
 
 @MODELS.register_module()
@@ -15,6 +16,7 @@ class DownSampler1D(nn.Module):
                  strides=2,
                  paddings=1,
                  out_indices=(0, 1, 2, 3),
+                 mask=False,
                  ):
         super(DownSampler1D, self).__init__()
 
@@ -25,6 +27,7 @@ class DownSampler1D(nn.Module):
         self.paddings = paddings
         self.out_channels = out_channels
         self.out_indices = out_indices
+        self.mask = mask
 
         td_layers = []
         for i in range(self.num_levels - 1):
@@ -59,12 +62,29 @@ class DownSampler1D(nn.Module):
     def forward(self, x):
         # x: N, C, 1, T
         x = x.squeeze(2)
+
+        if self.mask:
+            B, C, T = x.size()
+            mask = x.new_zeros((B, 1, T))
+            for feat_id, feat in enumerate(x):
+                # find valid feature length for each sample in the batch
+                # feat is of shape (C, T), we find the tails that are all zeros and take it as the padding
+                valid_feat_len = T - (feat == 0).all(dim=0).sum()
+                mask[feat_id, :, :valid_feat_len] = 1
+
         outs = []
         if 0 in self.out_indices:
             outs.append(x)
 
         for i, layer_name in enumerate(self.td_layers):
             x = self.td_layers[i](x)
+            if self.mask:
+                if self.td_layers[i].stride[0] > 1:
+                    # downsample the mask using nearest neighbor
+                    mask = F.interpolate(mask, size=x.size(-1), mode='nearest')
+
+                # masking out the features, stop grad to mask
+                x = x * mask.detach()
             if (i + 1) in self.out_indices:
                 outs.append(x)
         # outs: N, C, 1, T
