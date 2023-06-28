860627ba842cf6bc5ed8ebae4abf49878df46a8c
makecent <luchongkai96@gmail.com>
2023-06-28 08:18:52 +0800

    xx

--- configs/tadtr_f4x.py
+++ configs/tadtr_f4x.py
@@ -3,7 +3,11 @@ _base_ = [
 ]
 train_dataloader = dict(
     dataset=dict(feat_stride=4,
+                 window_size=256,
+                 window_stride=64,  # overlap=0.75
                  data_prefix=dict(feat='features/thumos_feat_ActionFormer_16input_4stride_2048/i3d_features')))
 val_dataloader = dict(
     dataset=dict(feat_stride=4,
+                 window_size=256,
+                 window_stride=64,  # overlap=0.75
                  data_prefix=dict(feat='features/thumos_feat_ActionFormer_16input_4stride_2048/i3d_features')))
