be756b8ac1cc4330bff190632e59db1355d73d12
makecent <luchongkai96@gmail.com>
2023-06-28 08:19:31 +0800

    xx

--- configs/tadtr_f4x.py
+++ configs/tadtr_f4x.py
@@ -9,5 +9,5 @@ train_dataloader = dict(
 val_dataloader = dict(
     dataset=dict(feat_stride=4,
                  window_size=256,
-                 window_stride=64,  # overlap=0.75
+                 window_stride=192,  # overlap=0.25
                  data_prefix=dict(feat='features/thumos_feat_ActionFormer_16input_4stride_2048/i3d_features')))
