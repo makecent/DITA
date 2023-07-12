f6dc6ee5aed68c701afd60885553f4bd5d34d50f
makecent <luchongkai96@gmail.com>
2023-07-13 00:35:52 +0800

    xx: support self-attention in decoder to be executed group-wisely. Note that this is just a testing. Code is ugly.

--- my_modules/layers/custom_layers.py
+++ my_modules/layers/custom_layers.py
@@ -436,6 +436,62 @@ class CustomDeformableDetrTransformerDecoderLayer(DeformableDetrTransformerDecod
         ]
         self.norms = ModuleList(norms_list)
 
+    def forward(self,
+                query: Tensor,
+                key: Tensor = None,
+                value: Tensor = None,
+                query_pos: Tensor = None,
+                key_pos: Tensor = None,
+                self_attn_mask: Tensor = None,
+                cross_attn_mask: Tensor = None,
+                key_padding_mask: Tensor = None,
+                **kwargs) -> Tensor:
+        query_pos_back = query_pos
+        query_, query_pos_ = query[:, :200, :], query_pos[:, :200, :]
+        dn_query, dn_query_pos = query[:, 200:, :], query_pos[:, 200:, :]
+        query_groups = torch.split(query_, 50, dim=1)
+        query_pos_groups = torch.split(query_pos_, 50, dim=1)
+        querys_ = []
+        for i in range(4):
+            query = query_groups[i]
+            query_pos = query_pos_groups[i]
+            query = self.self_attn(
+                query=query,
+                key=query,
+                value=query,
+                query_pos=query_pos,
+                key_pos=query_pos,
+                # attn_mask=self_attn_mask,
+                **kwargs)
+            querys_.append(query)
+        query = dn_query
+        query_pos = dn_query_pos
+        dn_query = self.self_attn(
+            query=query,
+            key=query,
+            value=query,
+            query_pos=query_pos,
+            key_pos=query_pos,
+            # attn_mask=self_attn_mask,
+            **kwargs)
+        query = torch.cat(querys_ + [dn_query], dim=1)
+        query_pos = query_pos_back
+
+        query = self.norms[0](query)
+        query = self.cross_attn(
+            query=query,
+            key=key,
+            value=value,
+            query_pos=query_pos,
+            key_pos=key_pos,
+            attn_mask=cross_attn_mask,
+            key_padding_mask=key_padding_mask,
+            **kwargs)
+        query = self.norms[1](query)
+        query = self.ffn(query)
+        query = self.norms[2](query)
+
+        return query
 
 class CustomDeformableDetrTransformerEncoderLayer(DeformableDetrTransformerEncoderLayer):
 
